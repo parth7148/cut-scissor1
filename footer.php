93f32baff7528104ac32107225c0795ac7e30450
hksavaliya <46381211+hksavaliya@users.noreply.github.com>
2019-01-09 17:41:21 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,44 @@
+<div class="container-fluid whiteBg animatedParent topPadding" >
+<section class="container-fluid yBand text-center animated bounceInUp">
+<p><img src="public/images/Footer_Logo.svg"></p>
+<p>The Exclusive Unisex Salon in your town acquainted with world-class tools and professionals for stunning looks and absolute luxury. </p>
+<p class="fshare-link"><a href="https://www.facebook.com/Looksunisexsalon/" class="fb" target="_blank"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a>
+<a href="https://twitter.com/LooksSalonIndia" class="tw" target="_blank"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
+<!--<a href="#" class="lin" target="_blank"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a>
+<a href="#" class="youtube" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a> -->
+<a href="https://www.instagram.com/looksunisexsalon/"  class="instagram" target="_blank"><i class="fa fa-instagram lg" aria-hidden="true"></i></a></p>
+<ul class="fLink container">
+    	<li><a href="index.html">Home</a></li>
+        <li><a href="aboutus.html">About Us</a></li>
+        <li><a href="ladies-salon-services.html">Services </a></li>
+        <li><a href="hair-styles-fashion-collection.html">Collection</a></li>
+        <li><a href="franchise.html">Franchise</a></li>
+        <li><a href="hair-dressing-beauty-makeup-courses-academy.html">Academy</a></li>
+        <li><a href="salon-beauty-products-eshop.html">e-Shop</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="salon-book-appointment.html">Book Appointment</a></li>
+       <li><a href="locate-salon.html">Salon Finder</a></li>
+       <li><a href="offers.html">Offers</a></li>
+  </ul>
+  <div class="clearfix"></div>
+</section>
+</div>
+<div class="animatedParent container-fluid whiteBg">
+<footer class="container ">
+
+    <div class="equalWidth animated bounceInDown">&copy; 2018, Looks Salon. All rights reserved.</div>
+    
+      <div class="equalWidth">
+      	<ul class="animated bounceInDown">
+        <li><a href="javascript:void(0)">Privacy Policy</a></li>
+         <li><a href="javascript:void(0)">Disclaimer</a></li>
+        	<li><a href="sitemap.html">Site Map</a></li>
+           
+            
+        </ul>
+      </div>
+       <div class="equalWidth text-right animated bounceInDown" style="display: none" ><a href="https://www.finessse.digital/" target="_blank" rel="nofollow"><img src="public/images/fisplLogo.png"></a></div>
+      <div class="clearfix"></div>
+   
+</footer>
+</div>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+
+<!-- Mirrored from www.lookssalon.in/franchise.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 25 Dec 2018 05:29:29 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<link href="public/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Looks Salon Franchise, India Cost | Award-winning Hair, Beauty Salon </title/>
+<meta name="description" content="">
+<meta name="keywords" content=""/>
+<link rel="stylesheet" href="public/css/bootstrap.css">
+<link rel="stylesheet" href="public/css/global.css">
+<link rel="stylesheet" href="public/css/font-awesome.min.css">
+<link rel="stylesheet" href="public/css/jquery.bxslider.css">
+ <link rel="stylesheet" href="public/css/lightbox.min.css">
+ <!-- For Google Map -->
+ 
+	
+	
+ <!-- End Google Map -->
+ 
+<link href="public/css/ipadad05.css?v=001" rel="stylesheet" type="text/css" media="screen and (max-width:1200px)" />
+<link href="public/css/mobilead05.css?v=001" rel="stylesheet" type="text/css" media="screen and (max-width: 700px)" />
+ <link rel="stylesheet" href="public/css/animations.css" type="text/css" media="screen and (min-width: 768px)">
+ <link rel="stylesheet" href="../code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109651724-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109651724-1');
+</script>
+<script type="text/javascript">
+    window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='../rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'ebd4fd1c016d4bba7539fa5c733ac02a3db5f404');
+</script>
+ <script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "HairSalon",
+  "name" : "Looks Salon",
+  "url" : "http://www.lookssalon.in/",
+  "email": "mailto:customercare@lookssalon.in",
+  "telephone": "+91-11-2875-1040",
+  "logo": "http://www.lookssalon.in/public/images/Footer_Logo.svg",
+  "image": "http://www.lookssalon.in/public/images/franchise.jpg",
+  "sameAs" : [ "https://www.facebook.com/Looksunisexsalon/",
+               "https://twitter.com/looksindia",
+		"https://www.instagram.com/looksunisexsalon/" ]
+
+}
+
+</script> 
+</head>
